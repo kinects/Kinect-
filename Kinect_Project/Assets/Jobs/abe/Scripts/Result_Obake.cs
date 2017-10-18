58189210da971bf7e47b2ed9fbf30c0ebf007a81
阿部広 <sigunaty@gmail.com>
2017-10-18 15:14:53 +0900

    タイトルとリザルト画面修正

---
+++ Kinect_Project/Assets/Jobs/abe/Scripts/Result_Obake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Result_Obake : MonoBehaviour {
+
+    public int cnt = 0;     //お化けの上下を管理する関数
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+
+        cnt++;
+
+        if(cnt <= 30)    //上に移動
+        {
+            pos.y += 0.01f;
+        }
+        if(cnt > 30)
+        {
+            pos.y -= 0.01f;
+        }
+        if(cnt == 60)
+        {
+            cnt = 0;
+        }
+
+        transform.position = pos;
+	}
+}
---
+++ Kinect_Project/Assets/Jobs/abe/Scripts/Result_Pumpkin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Result_Pumpkin : MonoBehaviour {
+
+    public GameObject face;             //カボチャの顔
+    public float color_time = 0;              //色を変化させる時間
+
+    // Use this for initialization
+    void Start () {
+        face = GameObject.Find("pumpkin_face");
+        face.GetComponent<Renderer>().material.color = new Color(255, 255, 0, 1.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        color_time += Time.deltaTime;
+
+        if (color_time > 1)
+        {
+            face.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+            color_time = 0;
+        }
+    }
+}
