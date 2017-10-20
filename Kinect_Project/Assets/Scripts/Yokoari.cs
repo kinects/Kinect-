1fdf64965bbb23544f1b774014e69f7430d90c45
DA\nwuser <goroumakisimamu810@gmail.com>
2017-10-20 11:27:06 +0900

    ヨコアリくんの切り替えができるぜ

--- Kinect_Project/Assets/Scripts/Yokoari.cs
+++ Kinect_Project/Assets/Scripts/Yokoari.cs
@@ -26,20 +26,6 @@ public class Yokoari : MonoBehaviour
     public bool squatState = false;
     public bool supriseState = false;
 
-    //ヨコアリくんのモデル切り替えのやつ
-    public int indexTrg = 0;            //0 通常 1 ヴァンパイア 2 かぼちゃ 3 花
-
-    //透過度
-    float alpha;
-    float alpha02;
-    float alpha03;
-    float alpha04;
-
-    //色のやつ
-    float red, green, blue;
-    float red02, green02, blue02;
-    float red03, green03, blue03;
-
     private Vector2 Position;
     private SpriteRenderer spRenderer;
     public static bool Hockenswitch = false;
@@ -50,9 +36,6 @@ public class Yokoari : MonoBehaviour
 
         //変数にアニメーターを入れる
         animator = GetComponent(typeof(Animator)) as Animator;
-
-        //マテリアルの色を取得
-        RendColor();
 
         //最も近かったオブジェクトを取得
         nearObj = serchTag(gameObject, "Ghost");
@@ -66,31 +49,7 @@ public class Yokoari : MonoBehaviour
         //アニメーションを再生する
         Animation();
 
-        switch (indexTrg)
-        {
-            case 0: //通常
-                    GetComponent<Renderer>().materials[0].color = new Color(red, green, blue, alpha);
-                    break;
-            case 1:
-                    //ヴァンパイア
-                    GetComponent<Renderer>().materials[0].color = new Color(red, green, blue, alpha02);
-                    break;
-            case 2:
-                    //かぼちゃ
-                    GetComponent<Renderer>().materials[0].color = new Color(red, green, blue, alpha03);
-                    break;
-            case 3:
-                    //花
-                    GetComponent<Renderer>().materials[0].color = new Color(red, green, blue, alpha04);
-                    break;
-            default:
 
-                    
-                    break;
-        }
-
-        //各モデルの透明度を制御
-        IndexAlpha();
 
 
         time += Time.deltaTime;
@@ -167,43 +126,174 @@ public class Yokoari : MonoBehaviour
 
     void aveBat()
     {
-        Vector3 ypos = GameObject.Find("Yokoari").transform.position;
-
-        if (Spone.BatLcnt > 0)
+        switch(FindObjectOfType<YokoariChange>().indexTrg)
         {
-            if (Vector2.Distance(GameObject.Find("BatL3D(Clone)").transform.position, transform.position) < 6.5f)
-            {
-                ypos.z = 10;
-                Hockenswitch = true;
-            }
-            else
-            {
-                if (Smoke.trgsSmoke == false)
-                {
-                    ypos.z = 10;
-                    Hockenswitch = false;
-                }
-            }
-        }
-        if (Spone.BatRcnt > 0)
-        {
-            if (Vector2.Distance(GameObject.Find("BatR3D(Clone)").transform.position, transform.position) < 6.5f)
-            {
-                ypos.z = 10;
-                Hockenswitch = true;
-            }
-            else
-            {
-                if (Smoke.trgsSmoke == false)
-                {
-                    ypos.z = 10;
-                    Hockenswitch = false;
-                }
-            }
-        }
 
-        GameObject.Find("Yokoari").transform.position = ypos;
-        Position = transform.position;
+            case 0:
+                Vector3 ypos = GameObject.Find("Yokoari").transform.position;
+
+                if (Spone.BatLcnt > 0)
+                {
+                    if (Vector2.Distance(GameObject.Find("BatL3D(Clone)").transform.position, transform.position) < 6.5f)
+                    {
+                        ypos.z = 10;
+                        Hockenswitch = true;
+                    }
+                    else
+                    {
+                        if (Smoke.trgsSmoke == false)
+                        {
+                            ypos.z = 10;
+                            Hockenswitch = false;
+                        }
+                    }
+                }
+                if (Spone.BatRcnt > 0)
+                {
+                    if (Vector2.Distance(GameObject.Find("BatR3D(Clone)").transform.position, transform.position) < 6.5f)
+                    {
+                        ypos.z = 10;
+                        Hockenswitch = true;
+                    }
+                    else
+                    {
+                        if (Smoke.trgsSmoke == false)
+                        {
+                            ypos.z = 10;
+                            Hockenswitch = false;
+                        }
+                    }
+                }
+
+                GameObject.Find("Yokoari").transform.position = ypos;
+                Position = transform.position;
+                break;
+                //--------------------------------------------------------------------------------------//
+            case 1:
+
+                Vector3 ypos02 = GameObject.Find("Vamp3D").transform.position;
+
+                if (Spone.BatLcnt > 0)
+                {
+                    if (Vector2.Distance(GameObject.Find("BatL3D(Clone)").transform.position, transform.position) < 6.5f)
+                    {
+                        ypos02.z = 10;
+                        Hockenswitch = true;
+                    }
+                    else
+                    {
+                        if (Smoke.trgsSmoke == false)
+                        {
+                            ypos02.z = 10;
+                            Hockenswitch = false;
+                        }
+                    }
+                }
+                if (Spone.BatRcnt > 0)
+                {
+                    if (Vector2.Distance(GameObject.Find("BatR3D(Clone)").transform.position, transform.position) < 6.5f)
+                    {
+                        ypos02.z = 10;
+                        Hockenswitch = true;
+                    }
+                    else
+                    {
+                        if (Smoke.trgsSmoke == false)
+                        {
+                            ypos02.z = 10;
+                            Hockenswitch = false;
+                        }
+                    }
+                }
+
+                GameObject.Find("Vamp3D").transform.position = ypos02;
+                Position = transform.position;
+                break;
+            //--------------------------------------------------------------------------------------//
+            case 2:
+
+                Vector3 ypos03 = GameObject.Find("Pumpkin3D").transform.position;
+
+                if (Spone.BatLcnt > 0)
+                {
+                    if (Vector2.Distance(GameObject.Find("BatL3D(Clone)").transform.position, transform.position) < 6.5f)
+                    {
+                        ypos03.z = 10;
+                        Hockenswitch = true;
+                    }
+                    else
+                    {
+                        if (Smoke.trgsSmoke == false)
+                        {
+                            ypos03.z = 10;
+                            Hockenswitch = false;
+                        }
+                    }
+                }
+                if (Spone.BatRcnt > 0)
+                {
+                    if (Vector2.Distance(GameObject.Find("BatR3D(Clone)").transform.position, transform.position) < 6.5f)
+                    {
+                        ypos03.z = 10;
+                        Hockenswitch = true;
+                    }
+                    else
+                    {
+                        if (Smoke.trgsSmoke == false)
+                        {
+                            ypos03.z = 10;
+                            Hockenswitch = false;
+                        }
+                    }
+                }
+
+                GameObject.Find("Pumpkin3D").transform.position = ypos03;
+                Position = transform.position;
+                break;
+            //--------------------------------------------------------------------------------------//
+            case 3:
+
+                Vector3 ypos04 = GameObject.Find("Flower3D").transform.position;
+
+                if (Spone.BatLcnt > 0)
+                {
+                    if (Vector2.Distance(GameObject.Find("BatL3D(Clone)").transform.position, transform.position) < 6.5f)
+                    {
+                        ypos04.z = 10;
+                        Hockenswitch = true;
+                    }
+                    else
+                    {
+                        if (Smoke.trgsSmoke == false)
+                        {
+                            ypos04.z = 10;
+                            Hockenswitch = false;
+                        }
+                    }
+                }
+                if (Spone.BatRcnt > 0)
+                {
+                    if (Vector2.Distance(GameObject.Find("BatR3D(Clone)").transform.position, transform.position) < 6.5f)
+                    {
+                        ypos04.z = 10;
+                        Hockenswitch = true;
+                    }
+                    else
+                    {
+                        if (Smoke.trgsSmoke == false)
+                        {
+                            ypos04.z = 10;
+                            Hockenswitch = false;
+                        }
+                    }
+                }
+
+                GameObject.Find("Flower3D").transform.position = ypos04;
+                Position = transform.position;
+                break;
+        }
+        //--------------------------------------------------------------------------------------//
+
     }
 
     void Animation()
@@ -231,59 +321,6 @@ public class Yokoari : MonoBehaviour
 
     }
 
-    void RendColor()
-    {
 
-            red = GetComponent<Renderer>().materials[0].color.r;
-            green = GetComponent<Renderer>().materials[0].color.g;
-            blue = GetComponent<Renderer>().materials[0].color.b;
-
-            red02 = GetComponent<Renderer>().materials[1].color.r;
-            green02 = GetComponent<Renderer>().materials[1].color.g;
-            blue02 = GetComponent<Renderer>().materials[1].color.b;
-
-            red03 = GetComponent<Renderer>().materials[2].color.r;
-            green03 = GetComponent<Renderer>().materials[2].color.g;
-            blue03 = GetComponent<Renderer>().materials[2].color.b;
-
-    }
-
-    void IndexAlpha()
-    {
-        switch (indexTrg)
-        {
-            case 0: //通常
-                alpha = 255;
-                alpha02 = 0;
-                alpha03 = 0;
-                alpha04 = 0;
-                break;
-            case 1:
-                //ヴァンパイア
-                alpha = 0;
-                alpha02 = 255;
-                alpha03 = 0;
-                alpha04 = 0;
-                break;
-            case 2:
-                //かぼちゃ
-                alpha = 0;
-                alpha02 = 0;
-                alpha03 = 255;
-                alpha04 = 0;
-                break;
-            case 3:
-                //花
-                alpha = 0;
-                alpha02 = 0;
-                alpha03 = 0;
-                alpha04 = 255;
-                break;
-            default:
-
-
-                break;
-        }
-    }
 
 }
---
+++ Kinect_Project/Assets/Scripts/YokoariChange.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YokoariChange : MonoBehaviour {
+
+    public GameObject normal;
+    public GameObject vamp;
+    public GameObject pumpkin;
+    public GameObject flower;
+
+    //ヨコアリくんの状態
+    public int indexTrg = 0;    //0:通常 1:ヴァンパイア 2:かぼちゃ 3:花
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch (indexTrg)
+        {
+            case 0: 
+                normal.SetActive(true);
+                vamp.SetActive(false);
+                pumpkin.SetActive(false);
+                flower.SetActive(false);
+                break;
+
+            case 1:
+                normal.SetActive(false);
+                vamp.SetActive(true);
+                pumpkin.SetActive(false);
+                flower.SetActive(false);
+                break;
+
+            case 2:
+                normal.SetActive(false);
+                vamp.SetActive(false);
+                pumpkin.SetActive(true);
+                flower.SetActive(false);
+                break;
+
+            case 3:
+                normal.SetActive(false);
+                vamp.SetActive(false);
+                pumpkin.SetActive(false);
+                flower.SetActive(true);
+                break;
+
+        }
+		
+	}
+}
